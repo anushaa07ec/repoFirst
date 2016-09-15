a5aeb70ae8cc5326113db8a7f8cf311fc27b1135
Raviteja <teja.22060@gmail.com>
2016-09-15 22:47:30 +0530

    add some thing in java class

--- FirstProject/First.java
+++ FirstProject/First.java
@@ -1,8 +1,11 @@
 public class First{
 public static void main(String args[]){
+
 System.out.println("this is first project");
-
+	System.out.println("this is the second sysout"+sample2Method());
 }
-
+String static sample2Method(){
+	return "this is return sttring";
+}
 
 }
